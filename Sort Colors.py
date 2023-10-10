99d8d48c4dac7b853fd44d48b32e865cb351a780
Hope Alemayehu <82286144+Hope-Alemayehu@users.noreply.github.com>
2023-10-10 10:33:01 +0300

    Create Sort Colors.py

---
+++ Sort Colors.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        
+        for i in range(len(nums)):
+            minIdx=i
+            for j in range(i+1,len(nums)):
+                if nums[j]<nums[minIdx]:
+                    minIdx=j
+                    
+            nums[i],nums[minIdx]=nums[minIdx],nums[i]
+                   
+        return nums
+    
